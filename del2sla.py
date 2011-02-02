b987c3f67bc9a7c6be53cf2d9d64d6062a2d7aba
Pete Zaitcev <zaitcev@yahoo.com>
2011-02-01 22:32:55 -0700

    First pass at working importer. File back-end only, naturally.

--- del2sla.py
+++ del2sla.py
@@ -10,6 +10,10 @@
 
 import sys
 import time
+import os
+import errno
+import codecs
+(utf8_encode, utf8_decode, utf8_reader, utf8_writer) = codecs.lookup("utf-8")
 from xml.etree import ElementTree
 
 TAG = "del2sla"
@@ -18,11 +22,23 @@ def Usage():
     print >>sys.stderr, "Usage: "+TAG+" target_dir bookmarks.xml"
     sys.exit(2)
 
+# We are not aware of any specification, so it is unclear if tags are split
+# by space or whitespace. We assume space, to be locale-independent.
+# But this means that we include tabs and foreign whitespace into tags.
+def split_tags(tagstr):
+    tags = []
+    for t in tagstr.split(' '):
+        t = t.strip(' ')
+        if t != '':
+            tags.append(t)
+    return tags
+
 class AppError(Exception):
     pass
 
 #
-# The open database (any back-end in theory, hardcoded files for now)
+# The open database (any back-end in theory, hardcoded to files for now)
+# XXX files are very inefficient: 870 bookmarks from a 280 KB XML take 6 MB.
 #
 class TagBase:
     def __init__(self, dirname0):
@@ -34,8 +50,112 @@ class TagBase:
             d = dirname0[:-1]
         self.dirname = d
 
+        if not os.path.exists(self.dirname):
+            raise AppError("Does not exist: "+self.dirname)
+        if not os.path.isdir(self.dirname):
+            raise AppError("Not a directory: "+self.dirname)
+
+        self.tagdir = self.dirname+"/tags"
+        self.markdir = self.dirname+"/marks"
+
+    def open(self):
+        try:
+            os.mkdir(self.tagdir)
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise AppError(str(e))
+        try:
+            os.mkdir(self.markdir)
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise AppError(str(e))
+
+    def close(self):
+        pass
+
+    # The add1 constructs key from UNIX seconds.
+    def add1(self, timeint, title, url, note, tags):
+        fix = 0
+        while 1:
+            stampkey = "%010d.%02d" % (timeint, fix)
+            markname = stampkey
+            # special-case full seconds to make directories a shade faster
+            if fix == 0:
+                markname = "%010d" % timeint
+            try:
+                f = open(self.markdir+"/"+markname, "w+")
+            except IOError, e:
+                fix = fix + 1
+                # actually fix should always be zero for human-generated input
+                if fix >= 100:
+                    return
+                continue
+            break
+
+        # This is done because ElementTree throws Unicode strings at us.
+        # When we try to write these strings, UnicodeEncodeError happens.
+        f = utf8_writer(f)
+
+        # We write the key into the file in case we ever decide to batch marks.
+        f.write(stampkey)
+        f.write("\n")
+
+        f.write(title)
+        f.write("\n")
+        f.write(url)
+        f.write("\n")
+        f.write(note)
+        f.write("\n")
+
+        for t in tags:
+            f.write(" ")
+            f.write(t)
+        f.write("\n")
+
+        f.close()
+
+        for t in tags:
+            # 1. Read
+            try:
+                f = open(self.tagdir+"/"+t, "r")
+            except IOError, e:
+                f = None
+            if f != None:
+                # This can be a long read - tens of thousands of mark keys
+                tagbuf = f.read()
+                f.close()
+            else:
+                tagbuf = ''
+            # 2. Modify
+            # It would be more efficient to scan by hand instead of splitting,
+            # but premature optimization is the root etc.
+            if markname in split_tags(tagbuf):
+                continue
+            tagbuf = tagbuf+" "+markname
+            # 3. Write
+            try:
+                f = open(self.tagdir+"/"+t, "w")
+            except IOError, e:
+                continue
+            f.write(tagbuf)
+            f.close()
+
+def verify_tags(tagstr):
+    if "/" in tagstr:
+        return 0
+    if "\n" in tagstr:
+        return 0
+    return 1
+
+def verify_attr(attrstr):
+    if "\n" in attrstr:
+        return 0
+    return 1
+
 def do(dirname, xmlname):
     base = TagBase(dirname)
+
+    base.open()
 
     try:
         # Verify XML has UTF-8 encoding perhaps?
@@ -60,8 +180,8 @@ def do(dirname, xmlname):
         if url == None:
             continue
         # 'tag' is a string of space-separated tags
-        tags = et.attrib.get('tag')
-        if tags == None:
+        tagstr = et.attrib.get('tag')
+        if tagstr == None:
             continue
         # not sure what to do with hash and meta
         # 'hash' is MD5 digest of URL
@@ -70,6 +190,19 @@ def do(dirname, xmlname):
         note = et.attrib.get('extended')
         if note == None:
             note = ""
+
+        if not verify_attr(title):
+            raise AppError("Invalid title: `"+title+"'")
+        if not verify_attr(url):
+            raise AppError("Invalid URL: `"+url+"'")
+        if not verify_attr(note):
+            raise AppError("Invalid note: `"+note+"'")
+
+        if not verify_tags(tagstr):
+            raise AppError("Invalid tags: `"+tagstr+"'")
+        tags = split_tags(tagstr)
+        if tags == []:
+            continue
 
         #time="2010-12-10T08:04:46Z"
         timestr = et.attrib.get('time')
@@ -80,21 +213,22 @@ def do(dirname, xmlname):
         try:
             timeval = time.strptime(timestr, "%Y-%m-%dT%H:%M:%SZ")
         except ValueError, e:
-            # XXX This may be worth the trouble to diagnose somehow
-            # P3
-            print e
-            timeval = None
+            # We bug out on this because this case may be worth diagnosing.
+            # The error message has both format and unparsed date string.
+            raise AppError(str(e))
         if timeval == None:
             continue
 
         try:
             timeint = int(time.mktime(timeval))
         except (ValueError, OverflowError), e:
+            # XXX A user supplied Year 1900 or something like that.
             print e
             continue
 
-        # XXX base.add(timeint*xxxxx, url, tags)
+        base.add1(timeint, title, url, note, tags)
 
+    base.close()
 
 def main(args):
     argc = len(args)
