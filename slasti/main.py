4a0c913a54b919481ba2c54a73918ccfbf46f8b4
Pete Zaitcev <zaitcev@yahoo.com>
2011-02-22 00:01:39 -0700

    Add pagination.

--- slasti/main.py
+++ slasti/main.py
@@ -10,15 +10,96 @@ import time
 
 from slasti import AppError
 
+PAGESZ = 10
+
+BLACKSTAR = "&#9733;"
+WHITESTAR = "&#9734;"
+
+def page_back(mark):
+    mark = mark.pred()
+    if mark == None:
+        return None
+    # In all other cases, we'll return something, even if 1 entry back.
+    n = 1
+    while n < PAGESZ:
+        m = mark.pred()
+        if m == None:
+            return mark
+        mark = m
+        n += 1
+    return mark
+
+def page_anchor_html(mark, path, text):
+    if mark == None:
+        return '[-]'
+    (stamp0, stamp1) = mark.key()
+    return '[<a href="%s/page.%d.%02d">%s</a>]' % (path, stamp0, stamp1, text)
+
 def mark_anchor_html(mark, path, text):
     if mark == None:
         return '[-]'
     (stamp0, stamp1) = mark.key()
     return '[<a href="%s/mark.%d.%02d">%s</a>]' % (path, stamp0, stamp1, text)
 
-def page_mark_html(start_response, user, base, stamp0, stamp1):
-    start_response("200 OK", [('Content-type', 'text/plain')])
-    return ["Page not wokrie yet: ", str(stamp0)+"."+str(stamp1), "\r\n"]
+def page_mark_html(start_response, pfx, user, base, stamp0, stamp1):
+    mark_top = base.lookup(stamp0, stamp1)
+    if mark_top == None:
+        # We have to have at least one mark to display a page
+        start_response("404 Not Found", [('Content-type', 'text/plain')])
+        return ["Page not found: ", str(stamp0), str(stamp1), "\r\n"]
+
+    path = pfx+'/'+user['name']
+
+    start_response("200 OK", [('Content-type', 'text/html')])
+    output = ["<html><body>\n"]
+
+    # align=center does not match individual mark's style
+    output.append('<h1><a href="%s/">%s</a> / %s</h1>\n' % \
+             (path, user['name'], page_anchor_html(mark_top, path, BLACKSTAR)))
+
+    mark = mark_top
+    mark_next = None
+    n = 0
+    while n < PAGESZ:
+        (stamp0, stamp1) = mark.key()
+        datestr = time.strftime("%Y-%m-%d", time.gmtime(stamp0))
+
+        output.append("<p>%s %s " % \
+                      (datestr, mark_anchor_html(mark, path, WHITESTAR)))
+        output.append(mark.html())
+        output.append("</p>\n")
+
+        mark_next = mark.succ()
+        if mark_next == None:
+            break
+        mark = mark_next
+        n += 1
+
+    output.append("<hr />\n")
+    output.append(page_anchor_html(page_back(mark_top), path, "&laquo;"))
+    output.append(page_anchor_html(mark_top,            path, BLACKSTAR))
+    output.append(page_anchor_html(mark_next,           path, "&raquo;"))
+    output.append("<br />\n")
+
+    output.append("</body></html>\n")
+    return output
+
+def page_empty_html(start_response, pfx, user, base):
+    path = pfx+'/'+user['name']
+
+    start_response("200 OK", [('Content-type', 'text/html')])
+    output = ["<html><body>\n"]
+
+    output.append('<h1>')
+    output.append(user['name'])
+    output.append(' / [-]</h1>\n')
+
+    output.append("<hr />\n")
+    output.append('[-] [-] [-]')
+    output.append("<br />\n")
+
+    output.append("</body></html>\n")
+    return output
 
 def one_mark_html(start_response, pfx, user, base, stamp0, stamp1):
     mark = base.lookup(stamp0, stamp1)
@@ -42,38 +123,42 @@ def one_mark_html(start_response, pfx, user, base, stamp0, stamp1):
 
     output.append("<hr />\n")
     output.append(mark_anchor_html(mark.pred(), path, "&laquo;"))
-    output.append(mark_anchor_html(mark,        path, "&#9734;"))
+    output.append(mark_anchor_html(mark,        path, WHITESTAR))
     output.append(mark_anchor_html(mark.succ(), path, "&raquo;"))
     output.append("<br />\n")
 
     output.append("</body></html>\n")
     return output
 
-# XXX This is temporary. We'll do pages at root when we have pages.
 def root_mark_html(start_response, pfx, user, base):
-    path = pfx+'/'+user['name']
+    mark = base.first()
+    if mark == None:
+        return page_empty_html(start_response, pfx, user, base)
+    (stamp0, stamp1) = mark.key()
+    return page_mark_html(start_response, pfx, user, base, stamp0, stamp1)
 
-    response_headers = [('Content-type', 'text/html')]
-    start_response("200 OK", response_headers)
-    output = ["<html><body>\n"]
-
-    # align=center does not match individual mark's style
-    output.append('<h1>')
-    output.append(user['name'])
-    output.append('</h1>\n')
-
-    for mark in base:
-        (stamp0, stamp1) = mark.key()
-        datestr = time.strftime("%Y-%m-%d", time.gmtime(stamp0))
-
-        output.append("<p>%s %s " % \
-                      (datestr, mark_anchor_html(mark, path, "&#9734;")))
-        output.append(mark.html())
-        output.append("</p>\n")
-
-    output.append("</body></html>\n")
-
-    return output
+    ## The non-paginated version
+    #
+    # response_headers = [('Content-type', 'text/html')]
+    # start_response("200 OK", response_headers)
+    # output = ["<html><body>\n"]
+    #
+    # # align=center does not match individual mark's style
+    # output.append('<h1>')
+    # output.append(user['name'])
+    # output.append('</h1>\n')
+    #
+    # for mark in base:
+    #     (stamp0, stamp1) = mark.key()
+    #     datestr = time.strftime("%Y-%m-%d", time.gmtime(stamp0))
+    #
+    #     output.append("<p>%s %s " % \
+    #                   (datestr, mark_anchor_html(mark, path, WHITESTAR)))
+    #     output.append(mark.html())
+    #     output.append("</p>\n")
+    #
+    # output.append("</body></html>\n")
+    # return output
 
 # full_mark_html() would be a Netscape bookmarks file, perhaps.
 def full_mark_xml(start_response, user, base):
@@ -142,6 +227,6 @@ def app(start_response, pfx, user, base, reqpath):
         if p[0] == "mark":
             return one_mark_html(start_response, pfx, user, base, stamp0, stamp1)
         if p[0] == "page":
-            return page_mark_html(start_response, user, base, stamp0, stamp1)
+            return page_mark_html(start_response, pfx, user, base, stamp0, stamp1)
         start_response("404 Not Found", [('Content-type', 'text/plain')])
         return ["Not found: ", reqpath, "\r\n"]
