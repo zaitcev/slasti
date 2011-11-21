c148ce204b24c0155962c876c248e615ef707e48
Pete Zaitcev <zaitcev@yahoo.com>
2011-11-20 20:59:50 -0700

    Add the "Preload" button
    When I started using Slasti from a tablet, it turned out that copying
    of random text for the link title was impossible. So, to provide a
    small assistance, we add a "Preload" button to the new mark form.
    Hitting this button pre-loads the title, using the URL field.
    
    Unfortunately, we found no way to get client to load a random URL,
    due to XSS protections. So, we use a server-side indirection,
    carefuly. It may yet turn out to be problematic, so suggestions
    are welcome.
    
    This patch adds a JavaScript component, in a separate file.
    Installation instructions adjusted accordingly.

---
+++ edit.js
@@ -0,0 +1,58 @@
+// <!-- CDATA? Laffo. -->
+// <!-- <script type="text/javascript"> -->
+
+function preload_title(fetch_url, title_id, button_id) {
+    // XXX Disable the button while request is outstanding
+    // XXX Add a timeout
+
+    // Fetch the user input from the form
+    var tgt_url = document.editform.href.value;
+    var url = fetch_url + "?url=" + encodeURIComponent(tgt_url);
+
+    // Get the reference to the result
+    var t = document.getElementById(title_id);
+    var b = document.getElementById(button_id);
+
+    // Legacy DOM:
+    // document.editform.title1
+    // document.forms.editform.title1
+    // document.forms["editform"].title1
+
+    // XXX verify that url at least starts with an HTTP scheme and "://"
+
+    // Screw IE6, just use the decent way.
+    var req = new XMLHttpRequest();
+    var title_str = null;
+    var timer = null;
+
+    function state_handler() {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+               // No need to check req.getResponseHeader("Content-Type") for us.
+               t.value = req.responseText;
+            }
+            // Not saving req.statusText for UI predictability.
+            if (timer)
+               clearTimeout(timer);
+            b.disabled = false;
+        }
+    }
+
+    function req_timeout() {
+        req.abort();
+        alert("abort");
+        b.disabled = false;
+    }
+
+    // Actually Window.setTimeout()
+    // 5s more than server-side. Just so.
+    timer = setTimeout(req_timeout, 30*1000);
+    b.disabled = true;
+
+    req.onreadystatechange = state_handler;
+    req.open("GET", url);
+    req.send(null);
+
+    // alert(title_ref.value);
+    return false;
+}
